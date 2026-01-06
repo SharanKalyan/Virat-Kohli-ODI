37a7dc4f9596270db76e7b1ad2bed34ae2018c76
Sharan Kalyan <sharandhoni2526@gmail.com>
2026-01-06 22:46:37 +0530

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def map_columns(ml):
+    df = ml.copy()
+
+    # Enforce MM/DD/YYYY
+    df['Date'] = pd.to_datetime(
+        df['Date'],
+        format='%m/%d/%Y',
+        errors='coerce'
+    )
+
+    df['Month'] = df['Date'].dt.month
+    df['Year'] = df['Date'].dt.year
+    df.drop('Date', inplace=True, axis=1)
+
+    df['M/Inns'] = df['M/Inns'].map({
+        '1st': 1,
+        '2nd': 2,
+        'N/A - No Result': 0
+    })
+
+    df['Captain'] = df['Captain'].map({
+        'Yes': 1,
+        'No': 0
+    })
+
+    return df
