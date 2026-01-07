2d594e7e2c3d67427cc220022941fe130377039f
Sharan Kalyan <sharandhoni2526@gmail.com>
2026-01-07 12:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,23 +42,23 @@ def add_bg_from_local(image_file):
         }}
 
         html, body, [class*="css"], p, span, label, div {{
-            color: #000000 !important;
+            color: #121212 !important;
         }}
 
         h1, h2, h3, h4, h5, h6 {{
-            color: #000000 !important;
+            color: #121212 !important;
         }}
 
         input, textarea {{
-            color: #000000 !important;
+            color: #121212 !important;
         }}
 
         div[data-baseweb="select"] span {{
-            color: #000000 !important;
+            color: #121212 !important;
         }}
 
         details summary {{
-            color: #000000 !important;
+            color: #121212 !important;
         }}
         </style>
         """,
